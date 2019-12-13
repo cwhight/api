dbbf43a9352280328a3b265e42fab26bc7a0d4ea
Charlie Whight <cdwhight@gmail.com>
2019-12-13 13:50:52 +0000

    added index and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ require 'date'
 10.times do
   Document.create( title: i, content: Faker::Lorem.sentence, created_at: DateTime.now - 3 )
   doc = Document.last
-
+  Revision.create(content: Faker::Lorem.sentence, created_at: DateTime.now - 2, document: doc)
+  Revision.create(content: Faker::Lorem.sentence, created_at: DateTime.now - 1, document: doc)
   i += 1
 end
